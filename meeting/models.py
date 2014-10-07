b5505c15b71b48b1b90e7fac6fd03138473c6272
scavedo159 <scavedo159@gmail.com>
2014-10-07 09:59:06 -0400

    added models for notes, files, and calendar

--- meeting/models.py
+++ meeting/models.py
@@ -1,14 +1,58 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class UserProfile(models.Model):
-    # This line is required. Links UserProfile to a User model instance.
-    user = models.OneToOneField(User)
 
-    # The additional attributes we wish to include.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
     picture = models.ImageField(upload_to='profile_images', blank=True)
 
-    # Override the __unicode__() method to return out something meaningful!
     def __unicode__(self):
         return self.user.username
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=128)
+    users = models.ManyToManyField(UserProfile)
+    completed = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Notes(models.Model):
+    project = models.ForeignKey('Project')
+    date_added = models.DateTimeField()
+    title = models.CharField(max_length=128)
+    content = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Notes"
+
+    def __unicode__(self):
+        return self.title
+
+
+class Files(models.Model):
+    project = models.ForeignKey('Project')
+    title = models.CharField(max_length=128)
+    date_added = models.DateTimeField()
+    file = models.FileField(upload_to='files')
+
+    class Meta:
+        verbose_name_plural = "Files"
+
+    def __unicode__(self):
+        return self.title
+
+
+class Calendar(models.Model):
+    project = models.ForeignKey('Project')
+    date_added = models.DateTimeField()
+    date_begin = models.DateTimeField()
+    date_end = models.DateTimeField()
+    title = models.CharField(max_length=128)
+    place = models.CharField(max_length=128)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.title
